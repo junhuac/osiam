c28859756a01f77a04ad16959e384822953a476d
Junhua Chang <junhuac@hotmail.com>
2016-02-04 15:53:03 -0800

    dev: change: Configure JOTM in Tomcat.

--- src/main/java/org/osiam/configuration/JOTMJtaConfiguration.java
+++ src/main/java/org/osiam/configuration/JOTMJtaConfiguration.java
@@ -2,47 +2,19 @@ package org.osiam.configuration;
 
 import org.objectweb.jotm.datasource.DataSourceFactory; 
 import org.objectweb.jotm.*; 
-import org.objectweb.jotm.UserTransactionService; 
-import org.objectweb.jotm.UserTransactionServiceImp; 
-import org.objectweb.transaction.jta.UserTransactionImp; 
-import org.objectweb.transaction.jta.UserTransactionManager; 
-import org.objectweb.transaction.jta.hibernate3.TransactionManagerLookup; 
+import org.objectweb.jotm.Current; 
+import org.objectweb.transaction.jta.TransactionManager; 
 import org.springframework.context.annotation.Bean; 
 import org.springframework.core.env.Environment; 
 import org.springframework.transaction.PlatformTransactionManager; 
 import org.springframework.transaction.jta.JtaTransactionManager; 
  
-import javax.transaction.TransactionManager; 
 import javax.transaction.UserTransaction; 
 import java.util.Enumeration; 
 import java.util.Properties; 
  
  
-public class JOTMJtaConfiguration implements TransactionConfig  { 
-  
-    public Properties tailorEntityManagerFactoryProperties(Properties properties) { 
-        properties.setProperty("hibernate.transaction.manager_lookup_class", 
-                TransactionManagerLookup.class.getName()); 
-        return properties; 
-    } 
- 
-    @Bean(destroyMethod = "shutdownForce") 
-    public UserTransactionService userTransactionService() { 
- 
-        UserTransactionService service= new UserTransactionServiceImp() ; 
-        Properties props= new Properties(); 
- 
-        props.put("org.objectweb.jotm.service","org.objectweb.jotm.standalone.UserTransactionServiceFactory"); 
-        props.put("org.objectweb.jotm.log_base_name","aaaaaa"); 
-        props.put("org.objectweb.jotm.output_dir","../standalone/log/"); 
-        props.put("org.objectweb.jotm.log_base_dir","../standalone/log/"); 
- 
-        service.init(props); 
- 
-        return service; 
- 
-    } 
- 
+public class JOTMJtaConfiguration{ 
     @Bean 
     public PlatformTransactionManager platformTransactionManager() throws Throwable { 
         UserTransaction userTransaction = this.userTransaction(); 
@@ -52,16 +24,22 @@ public class JOTMJtaConfiguration implements TransactionConfig  {
  
     @Bean 
     public UserTransaction userTransaction() throws Throwable { 
-        UserTransactionImp userTransactionImp = new UserTransactionImp(); 
-        userTransactionImp.setTransactionTimeout(60); 
-        return userTransactionImp; 
+    	  // Check for already active JOTM instance. 
+    	Current jotmCurrent = Current.getCurrent(); 
+    	 
+	    // If none found, create new local JOTM instance. 
+	    if (jotmCurrent == null) { 
+	     // Only for use within the current Spring context: 
+	     // local, not bound to registry. 
+	     Jotm jotm = new Jotm(true, false); 
+	     jotmCurrent = Current.getCurrent(); 
+	    } 
+	   return jotmCurrent;
     } 
  
     @Bean(initMethod = "init", destroyMethod = "close") 
     public TransactionManager transactionManager() throws Throwable { 
-        UserTransactionManager userTransactionManager = new UserTransactionManager(); 
-        userTransactionManager.setForceShutdown(false); 
-        userTransactionManager.setStartupTransactionService(false); 
+        TransactionManager userTransactionManager = Current.getTransactionManager(); 
         return userTransactionManager; 
     } 
  
